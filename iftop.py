bbea144bb5cbd7c5936e7df14ac7dfbf11ac6880
ex3lite <sharashka.brk@gmail.com>
2024-03-27 19:29:10 +0800

    Update iftop.py

--- iftop.py
+++ iftop.py
@@ -8,9 +8,6 @@ def print_network_traffic():
     for connection in connections:
         local_address = connection.laddr
         remote_address = connection.raddr
-        while True:
-            print_network_traffic()
-            time.sleep(1)
         if remote_address:
             print(f"Локальный адрес: {local_address.ip}:{local_address.port}")
             print(f"Удаленный адрес: {remote_address.ip}:{remote_address.port}")
