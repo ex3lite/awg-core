ee0ebc32df5103177944892a1d440cd085074b9d
ex3lite <sharashka.brk@gmail.com>
2024-03-27 19:16:21 +0800

    Update iftop.py

--- iftop.py
+++ iftop.py
@@ -1,19 +1,20 @@
 import psutil
 import time
 
-def iftop():
+def iftop(interface):
     while True:
-        # Получаем информацию о сетевом трафике
-        net_io = psutil.net_io_counters(pernic=True)
+        # Получаем информацию о сетевом трафике для указанного интерфейса
+        io = psutil.net_io_counters(pernic=True)[interface]
 
-        print("Network Traffic:")
-        for interface, io in net_io.items():
-            print(f"Interface: {interface}")
-            print(f"  Sent: {io.bytes_sent} bytes")
-            print(f"  Received: {io.bytes_recv} bytes")
+        print("Network Traffic for", interface)
+        print(f"  Sent: {io.bytes_sent} bytes")
+        print(f"  Received: {io.bytes_recv} bytes")
 
         # Ждем некоторое время перед обновлением информации
         time.sleep(1)
 
-# Вызываем функцию iftop для отслеживания трафика
-iftop()
+# Укажите интерфейс, который вы хотите мониторить
+selected_interface = 'wg0'
+
+# Вызываем функцию iftop с указанным интерфейсом для отслеживания трафика
+iftop(selected_interface)
