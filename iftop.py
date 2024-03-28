a88eed99cd657e458d5515dc63b9023330588bc3
ex3lite <sharashka.brk@gmail.com>
2024-03-28 14:38:11 +0800

    Update iftop.py

--- iftop.py
+++ iftop.py
@@ -7,12 +7,13 @@ def iftop():
     for packet in scapy.sniff(iface="wg0", count=10):
         # Печать информации о IP-адресах
         print(packet[scapy.IP].src, packet[scapy.IP].dst)
-        # Печать информации о портах
-        print(packet[scapy.TCP].sport, packet[scapy.TCP].dport)
-        # Печать информации о размере пакета
-        print(len(packet))
-        # Печать информации о времени
-        print(packet.time)
+        if scapy.TCP in packet:
+            # Печать информации о портах
+            print(packet[scapy.TCP].sport, packet[scapy.TCP].dport)
+            # Печать информации о размере пакета
+            print(len(packet))
+            # Печать информации о времени
+            print(packet.time)
 
 # Бесконечный цикл
 while True:
