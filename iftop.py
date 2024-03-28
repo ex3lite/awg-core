5b9f4f7e3195f7dba8c572dfc64b060024faf0f7
ex3lite <sharashka.brk@gmail.com>
2024-03-28 14:26:59 +0800

    Update iftop.py

--- iftop.py
+++ iftop.py
@@ -6,7 +6,7 @@ time.sleep(1)  # Pause for 1 second before repeating the loop
 # Infinite loop to continuously monitor network traffic
 while True:
     # Execute the iftop command and get the output
-    output = subprocess.check_output(["iftop", "-n", "-N", "-t", "-s", "1"], universal_newlines=True)
+    output = subprocess.check_output(["iftop"], universal_newlines=True)
 
     # Process the output
     lines = output.split("\n")
